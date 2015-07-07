62dcadf529b8a1d5ce02846018d81fae84eedba2
Frode Petterson <frode.andre.petterson@gmail.com>
2015-07-07 13:16:15 +0200

    Initial commit.

---
+++ README.md
@@ -0,0 +1,24 @@
+H5P Mods
+==========
+
+This WordPress plugin is provided as an example of how you can use the actions
+provided by the H5P plugin. The actions are used for altering or adding
+H5P content or library resources.
+
+Feel free to fork this project or simply use it as a template for doing your
+own custom modifications to H5P.
+
+You should be aware that the hooks you add on your site will not follow
+the content if it's exported.
+
+## License
+
+(The MIT License)
+
+Copyright (c) 2015 Joubel AS
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
---
+++ h5pmods.php
@@ -0,0 +1,109 @@
+<?php
+/**
+ * H5P Mods Plugin.
+ *
+ * Alters the way the H5P plugin works.
+ *
+ * @package   H5P
+ * @author    Joubel <contact@joubel.com>
+ * @license   MIT
+ * @link      http://joubel.com
+ * @copyright 2015 Joubel
+ *
+ * @wordpress-plugin
+ * Plugin Name:       H5P Mods
+ * Plugin URI:        http://h5p.org/
+ * Description:       Allows you to alter how the H5P plugin works.
+ * Version:           0.0.1
+ * Author:            Joubel
+ * Author URI:        http://joubel.com
+ * Text Domain:       h5pmods
+ * License:           MIT
+ * License URI:       http://opensource.org/licenses/MIT
+ * Domain Path:       /languages
+ * GitHub Plugin URI: https://github.com/h5p/h5pmods-wordpress
+ */
+
+// If this file is called directly, abort.
+if (!defined('WPINC')) {
+  die;
+}
+
+/**
+ * Allows you to alter the H5P library semantics, i.e. changing how the
+ * editor looks and how content parameters are filtered.
+ *
+ * In this example we change the preview label for the collage tool where
+ * version is < 1.0.0.
+ *
+ * @param object &$semantics The same as in semantics.json
+ * @param string $name The machine readable name of the library.
+ * @param int $majorVersion First part of the version number.
+ * @param int $minorVersion Second part of the version number.
+ */
+function h5pmods_alter_semantics(&$semantics, $name, $majorVersion, $minorVersion) {
+  if ($name === 'H5P.Collage' && $majorVersion < 1) {
+    $semantics->label = 'Altered Label';
+  }
+}
+add_action('h5p_alter_library_semantics', 'h5pmods_alter_semantics', 10, 4);
+
+/**
+ * Allows you to alter the parameters of H5P content after it has been filtered
+ * through semantics. This is useful for adapting the content to the current
+ * context.
+ *
+ * In this example we add a paragraph to the question text on all the multiple
+ * choice tasks.
+ *
+ * @param object &$paramters The content input used to "start" the library.
+ * @param string $name The machine readable name of the library.
+ * @param int $majorVersion First part of the version number.
+ * @param int $minorVersion Second part of the version number.
+ */
+function h5pmods_alter_parameters(&$parameters, $name, $majorVersion, $minorVersion) {
+  if ($name === 'H5P.MultiChoice') {
+    $parameters->question .= '<p>Generated at ' . time() . '.</p>';
+  }
+}
+add_action('h5p_alter_filtered_parameters', 'h5pmods_alter_parameters', 10, 4);
+
+/**
+ * Allows you to alter which JavaScripts are loaded for H5P. This is
+ * useful for adding your own custom scripts or replacing existing once.
+ *
+ * In this example we're going add a custom script which keeps track of the
+ * scoring for drag 'n drop tasks.
+ *
+ * @param object &$scripts List of JavaScripts that will be loaded.
+ * @param array $libraries The libraries which the scripts belong to.
+ * @param string $embed_type Possible values are: div, iframe, external, editor.
+ */
+function h5pmods_alter_scripts(&$scripts, $libraries, $embed_type) {
+  if (isset($libraries['H5P.DragQuestion'])) {
+    $scripts[] = (object) array(
+      'path' => 'score-tracking.js', // Relative to wp-content/uploads/
+      'version' => '?ver=1.2.3' // Cache buster
+    );
+  }
+}
+add_action('h5p_alter_library_scripts', 'h5pmods_alter_scripts', 10, 3);
+
+/**
+ * Allows you to alter which stylesheets are loaded for H5P. This is
+ * useful for adding your own custom styles or replacing existing once.
+ *
+ * In this example we're going add a custom script which keeps track of the
+ * scoring for drag 'n drop tasks.
+ *
+ * @param object &styles List of stylesheets that will be loaded.
+ * @param array $libraries The libraries which the styles belong to.
+ * @param string $embed_type Possible values are: div, iframe, external, editor.
+ */
+function h5pmods_alter_styles(&$styles, $libraries, $embed_type) {
+  $styles[] = (object) array(
+    'path' => 'http://mydomain.org/custom-h5p-styling.css', // Absolute path
+    'version' => '?ver=1.3.7' // Cache buster
+  );
+}
+add_action('h5p_alter_library_scripts', 'h5pmods_alter_styles', 10, 3);
